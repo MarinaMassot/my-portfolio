a51964b6e2e2e549b5a7f4a78fcd8d3d5604d949
Marina <98844023+MarinaMassot@users.noreply.github.com>
2025-04-24 15:39:32 +0200

    initialize application

---
+++ app/components/Section.tsx
@@ -0,0 +1,7 @@
+import type { PropsWithChildren } from "react"
+
+export const Section = (props: PropsWithChildren) => {
+	return (
+		<section className="max-w-3xl px-4 m-auto">{props.children}</section>
+	)
+}
